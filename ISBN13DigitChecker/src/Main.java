e9253c1bfd3225a8afcd1338685100143e986afb
rsmalani <rushikesh.malani@gmail.com>
2023-01-21 22:36:09 +0530

    ISBN project second commit

--- ISBN13DigitChecker/src/Main.java
+++ ISBN13DigitChecker/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Are you ready to begin (Yes or No)?");
         String user_response = sc.nextLine();
-        if (user_response.equals("No")) {
+        if (!user_response.equals("Yes")) {
             System.out.println("Thank you, Good bye");
             System.exit(0);
         }
@@ -49,7 +49,7 @@ public class Main {
                 System.exit(0);
             }
             for (int i = 0; i < isbn13.length(); i++) {
-                char s = isbn.charAt(i);
+                char s = isbn13.charAt(i);
                 track1++;
                 if (track1 % 2 == 0) {
                     multiply1 = 3 * (sum_even1 = sum_even1 + Character.getNumericValue(s));
@@ -59,11 +59,16 @@ public class Main {
             }
             int remainder1 = (sum_odd1 + multiply1) % 10;
             if (remainder1 == 0)
-                System.out.println(Console_Colors.RED_BOLD_BRIGHT + "The given ISBN number is valid.");
+                System.out.println(Console_Colors.BLUE_BOLD_BRIGHT + "The given ISBN number is valid.");
             else
-                System.out.println(Console_Colors.BLUE_BOLD_BRIGHT + "The given ISBN number is invalid.");
+                System.out.println(Console_Colors.RED_BOLD_BRIGHT + "The given ISBN number is invalid.");
 
-
-        } while (user_response.equals("Yes"));
+            System.out.println(Console_Colors.RESET + "\n\nAre you ready to begin (Yes or No)?");
+            user_response = sc.nextLine();
+            if (!user_response.equals("Yes")) {
+                System.out.println("Thank you, Good bye");
+                System.exit(0);
+            }
+        } while (true);
     }
 }
